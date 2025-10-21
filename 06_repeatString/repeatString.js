71ebd539eed1b590449bf8281accc53396d85c10
laym00n <keshavgill030@gmail.com>
2025-10-21 12:16:35 +0530

    complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let string = ""
+    for (let i = 0; i < num; i++) {
+        string += text;
+    }
+    return string;
 };
 
 // Do not edit below this line
