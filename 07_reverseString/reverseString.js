9f8aab18e6f65cd73d291d93069891a8f290c286
laym00n <keshavgill030@gmail.com>
2025-10-21 12:28:20 +0530

    Complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let newText = text.split('');
+    const reverseArrayString = []
+    for (let i = newText.length - 1; i >= 0; i--) {
+        reverseArrayString.push(newText[i]);
+    }
+    return reverseArrayString.join('');
 };
 
 // Do not edit below this line
