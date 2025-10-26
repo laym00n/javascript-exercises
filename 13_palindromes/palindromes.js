d7f5df2d7a183f3d8f18bc0144b8c1bd260599c8
laym00n <keshavgill030@gmail.com>
2025-10-26 16:56:38 +0530

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphaNumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanString = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphaNumerical.includes(char))
+        .join('');
+
+    const reversedString = cleanString
+        .split('')
+        .reverse()
+        .join('');
+    
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
