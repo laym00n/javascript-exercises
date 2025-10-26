8ffd525b35739709aa2a76cdc2375ffbabc593c6
laym00n <keshavgill030@gmail.com>
2025-10-26 16:15:45 +0530

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	const result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	const result = num1 -num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let result = 0;
+  for (num of nums) {
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let result = 1;
+  for (num of nums) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	let result = 1;
+  for (let i = 0; i < expo; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
